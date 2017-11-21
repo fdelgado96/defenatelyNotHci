ee456257e61443f1fc88940809076889c0c6f03f
lreinaki@itba.edu.ar <lreinaki@itba.edu.ar>
2017-11-21 05:10:54 +0000

    NewRoom.java edited online with Bitbucket

--- tpe2/app/src/main/java/com/falopa/smarthome/views/NewRoom.java
+++ tpe2/app/src/main/java/com/falopa/smarthome/views/NewRoom.java
@@ -4,11 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.falopa.smarthome.R;
+import com.falopa.smarthome.utils.Callback;
+import com.falopa.smarthome.model.Room;
 
-public class NewRoom extends AppCompatActivity {
+public class NewRoom extends AppCompatActivity implements Callback {
+    private boolean creating = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,19 +32,50 @@ public class NewRoom extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
+        EditText editText = findViewById(R.id.new_room_name);
+        switch(item.getItemId()) {
             case R.id.action_create_room:
-                // CREATE ROOM
-                Toast.makeText(NewRoom.this, R.string.create_room_success, Toast.LENGTH_LONG).show();
-                // FALL INTO NEXT CASE
+                if(!creating) {
+                    creating = true;
+                    editText.setEnabled(false);
+                    Toast.makeText(NewRoom.this, R.string.create_room_wait, Toast.LENGTH_LONG).show();
+                    String name = editText.getText().toString();
+                    Room.create(name, this);
+                }
+                return true;
             case android.R.id.home:
-                // CLOSE ACTIVITY
-                this.finish();
+                if(!creating) {
+                    this.finish();
+                }
                 return true;
 
             default:
                 return super.onOptionsItemSelected(item);
-
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(!creating) {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public void onSuccess() {
+        EditText editText = findViewById(R.id.new_room_name);
+        //UPDATE ROOMS
+        creating = false;
+        editText.setEnabled(true);
+        Toast.makeText(NewRoom.this, R.string.create_room_success, Toast.LENGTH_LONG).show();
+        this.finish();
+    }
+
+    @Override
+    public void onFail() {
+        EditText editText = findViewById(R.id.new_room_name);
+        creating = false;
+        editText.setEnabled(true);
+        Toast.makeText(NewRoom.this, R.string.create_room_fail, Toast.LENGTH_LONG).show();
     }
 }
